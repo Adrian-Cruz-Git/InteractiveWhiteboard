513aaf2a718d8c4c4385075c779a83823285afaa
George <gcollier229@gmail.com>
2025-08-29 09:36:12 +1200

    New page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,20 +3,23 @@ import WhiteboardApp from "./pages/WhiteboardPage.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage.jsx";
 
 function App() {
   return (
     <Router>
       <nav>
-        <Link to="/">Landing</Link> | 
+        <Link to="/">Home</Link> | 
         <Link to="/login">Login</Link> | 
         <Link to="/whiteboard">Whiteboard</Link>
+        <Link to="/settings">Settings</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/whiteboard" element={<WhiteboardApp />} />
+        <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </Router>
   );
--- front-end/src/pages/LoginPage.jsx
+++ front-end/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 // Page for user authentication (login/register) - first page a user is met with
 // Option to create account or login 
+//Use firebase auth ui
 // Later integrate google authentication aswell (button)
 
 function LoginPage() {
