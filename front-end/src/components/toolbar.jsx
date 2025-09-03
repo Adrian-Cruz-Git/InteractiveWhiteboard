47de9273effb701deb564fe5ddcfddedda3d628b
Yuna <7baeyuna@gmail.com>
2025-09-03 19:39:13 +1200

    three out of eight icons added

--- front-end/src/components/toolbar.jsx
+++ front-end/src/components/toolbar.jsx
@@ -1,11 +1,20 @@
 import './toolbar.css';
+import cursorIcon from '../assets/cursor.png';
+import penIcon from '../assets/pen.png';
+import highlightIcon from '../assets/highlight.png';
 
 export default function Toolbar(){
     return(
         <div className="toolbar">
-            <button>Cursor</button>
-            <button>Pen</button>
-            <button>Highlight</button>
+            <button>
+                <img src={cursorIcon} alt="Cursor" style={{ width: '32px', height: '32px' }} />
+            </button>
+            <button>
+                <img src={penIcon} alt="Pen" style={{ width: '32px', height: '32px' }} />
+            </button>
+            <button>
+                <img src={highlightIcon} alt="Highlighter" style={{ width: '32px', height: '32px' }} />
+            </button>
             <button>Eraser</button>
             <button>Sticky Note</button>
             <button>Shape</button>
