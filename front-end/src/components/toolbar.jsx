b3cb77ec0effcb3b19fce22c402952d7be4abea4
Yuna <7baeyuna@gmail.com>
2025-09-05 07:55:46 +1200

    all toolbar icons complete + undo & redo button icon, removed outline when clicking on icons for clean clicks

--- front-end/src/components/toolbar.jsx
+++ front-end/src/components/toolbar.jsx
@@ -2,24 +2,47 @@ import './toolbar.css';
 import cursorIcon from '../assets/cursor.png';
 import penIcon from '../assets/pen.png';
 import highlightIcon from '../assets/highlight.png';
+import eraserIcon from '../assets/eraser.png';
+import stickyNoteIcon from '../assets/stickyNote.png';
+import shapesIcon from '../assets/shapes.png';
+import textIcon from '../assets/text.png';
+import imageIcon from '../assets/image.png';
+import undoIcon from '../assets/undo.png';
+import redoIcon from '../assets/redo.png';
 
 export default function Toolbar(){
     return(
         <div className="toolbar">
             <button>
-                <img src={cursorIcon} alt="Cursor" style={{ width: '32px', height: '32px' }} />
+                <img src={cursorIcon} alt="Cursor" style={{ width: '25px', height: '25px' }} />
             </button>
             <button>
-                <img src={penIcon} alt="Pen" style={{ width: '32px', height: '32px' }} />
+                <img src={penIcon} alt="Pen" style={{ width: '25px', height: '25px' }} />
             </button>
             <button>
-                <img src={highlightIcon} alt="Highlighter" style={{ width: '32px', height: '32px' }} />
+                <img src={highlightIcon} alt="Highlighter" style={{ width: '25px', height: '25spx' }} />
             </button>
-            <button>Eraser</button>
-            <button>Sticky Note</button>
-            <button>Shape</button>
-            <button>Text</button>
-            <button>Image</button>
+            <button>
+                <img src={eraserIcon} alt="Eraser" style={{ width: '25px', height: '25px' }} />
+            </button>
+            <button>
+                <img src={stickyNoteIcon} alt="Sticky Note" style={{ width: '25px', height: '25px' }} />    
+            </button>
+            <button>
+                <img src={shapesIcon} alt="Shapes" style={{ width: '25px', height: '25px' }} />
+            </button>
+            <button>
+                <img src={textIcon} alt="Text" style={{ width: '25px', height: '25px' }} />
+            </button>
+            <button>
+                <img src={imageIcon} alt="Image" style={{ width: '25px', height: '25px' }} />
+            </button>
+            <button>
+                <img src={undoIcon} alt="Undo" style={{ width: '25px', height: '25px' }} />
+            </button>
+            <button>
+                <img src={redoIcon} alt="Redo" style={{ width: '25px', height: '25px' }} />
+            </button>   
         </div>
     )
 }
