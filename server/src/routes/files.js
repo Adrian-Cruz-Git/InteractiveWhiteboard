1c9878b5db34aa9174b391276f87cdb3b70f077b
Adrian-Cruz-Git <qny9097@autuni.ac.nz>
2025-09-07 22:06:09 +1200

    changed it so that the upload route writes to firestore
    working with urls that are publicly accessable. i cant spell

--- server/src/routes/files.js
+++ server/src/routes/files.js
@@ -2,6 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const verifyJWT = require("../middleware/verifyJWT");
 const supabase = require("../config/supabase");
+const { db } = require("../config/firebase");
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
@@ -16,6 +17,14 @@ router.post("/upload", verifyJWT, upload.single("file"), async (req, res) => {
       .upload(filePath, file.buffer, { contentType: file.mimetype });
 
     if (error) throw error;
+
+    await db.collection("users").doc(req.user).collection("files").add({
+      name: file.originalname,
+      path: filePath,
+      type: file.mimetype,
+      size: file.size,
+      uploadedAt: new Date(),
+    });
 
     res.status(201).json({ path: filePath });
   } catch (err) {
