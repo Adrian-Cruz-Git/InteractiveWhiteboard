b08f45f7f09fd2871a62fd663aa396fe47b9aae5
Yuna <7baeyuna@gmail.com>
2025-09-08 21:32:42 +1200

    no comment

--- front-end/src/pages/WhiteboardPage.jsx
+++ front-end/src/pages/WhiteboardPage.jsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
+import { act, useState } from "react";
 import Navbar from "../components/Navbar";
 import Whiteboard from "../components/Whiteboard";
 import Toolbar from "../components/toolbar";
 
-function WhiteboardApp() {
+function WhiteboardApp(){
 
 
     const [boards, setBoards] = useState([{ id: 1, strokes: [] }]);
     const [activeBoard, setActiveBoard] = useState(1);
-    const [activeTool, setActiveTool] = useState('cursor'); // default to cursor/off
-
+    const [activeTool, setActiveTool] = useState("pen"); //pen
 
     const addBoard = () => {
         const newId = boards.length + 1;
@@ -28,7 +27,6 @@ function WhiteboardApp() {
     const activeBoardData = boards.find((b) => b.id === activeBoard);
 
     return (
-
         <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
             <Navbar
                 boards={boards}
@@ -37,18 +35,29 @@ function WhiteboardApp() {
                 onAddBoard={addBoard}
             />
 
-            <div style={{ flex: 1, display: "flex", background: "#fff", marginTop: "3rem" }}>
-                <Toolbar activeTool={activeTool} setActiveTool={setActiveTool} /> {/*new*/}
-                <div style={{ flex: 1, padding: "1rem" }}>
+            <div style={{ flex: 1, display: "flex", background: "#fff" }}>
+                <div style={{
+                    display: "flex",
+                    alignItems: "center", //vertically center
+                    height: "100vh",      //full viewport height
+                    position: "fixed",    //fix to left side
+                    left: 0,
+                    top: 0,
+                    zIndex: 10,
+                    padding: "1rem" //makes toolbar stick to wall or not
+                }}>
+                    <Toolbar activeTool={activeTool} setActiveTool={setActiveTool}  />
+                </div>
+                <div style={{ flex: 1, marginLeft: "80px", padding: "1rem" }}>
                     {activeBoardData && (
                         <div key={activeBoardData.id}>
                             <h2>Whiteboard {activeBoardData.id}</h2>
                             <Whiteboard
                                 strokes={activeBoardData.strokes}
-                                activeTool={activeTool}  // set active tool
                                 onChange={(newStrokes) =>
                                     updateStrokes(activeBoardData.id, newStrokes)
                                 }
+                                activeTool={activeTool}
                             />
                         </div>
                     )}
