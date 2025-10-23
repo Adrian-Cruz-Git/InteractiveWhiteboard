cb4a75fa872e239e7eea033a488dd14956e6a1d5
Adrian-Cruz-Git <qny9097@autuni.ac.nz>
2025-10-23 20:56:16 +1300

    commit shit fixing

--- server/src/config/supabase.js
+++ server/src/config/supabase.js
@@ -3,6 +3,15 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config("../../.env");
+
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
 if (!supabaseUrl) {
   throw new Error("Missing VITE_SUPABASE_URL. Check your .env file dont be a yuna");
 }
@@ -11,8 +20,6 @@ if (!supabaseAnonKey) {
   throw new Error("Missing VITE_SUPABASE_ANON_KEY. Check your .env file dumbasses");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: { persistSession: false },
-});
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 module.exports = { supabase };
